2db141f9268188b16898fdd4e7972d56f8cd20fd
Eleri <bala.eleri@capgemini.com>
2021-03-18 11:42:11 +0530

    Added Search Class

---
+++ src/main/java/com/qa/pages/Search.java
@@ -0,0 +1,17 @@
+package com.qa.pages;
+
+public class Search {
+
+	
+public void Search1() {
+		
+		System.out.println("Login Page");
+	}
+
+public void Search2() {
+	
+	System.out.println("Login Page");
+}
+
+
+}
